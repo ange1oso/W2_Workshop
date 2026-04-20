609dd9397ad750e9f7f107ead3da638857fc1fec
david ocampo <docampo@my.yearupunited.org>
2026-04-20 02:15:56 -0500

    New SQL file

---
+++ ocampo_Ex4A_insert_records.sql
@@ -0,0 +1,13 @@
+USE dogwalkingbusiness;
+
+INSERT INTO Client (ClientID, Name, PhoneNumber, Location, Age, NumberOfDogs)
+VALUES
+(1, 'Lana Rodriguez', '219-555-1023', 'Hammond, IN', 34, 2),
+(2, 'Michael Thompson', '219-555-2847', 'Munster, IN', 42, 1),
+(3, 'Sarah Nguyen', '773-555-6612', 'Highland, IN', 29, 3),
+(4, 'James Walker', '312-555-9901', 'East Chicago, IN', 51, 1),
+(5, 'Emily Carter', '219-555-4309', 'Griffith, IN', 25, 2);
+
+Select *
+FROM Client
+
